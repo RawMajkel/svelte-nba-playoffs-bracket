749bad87bc559474426d6849681690352a3364d8
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-16 12:30:12 +0200

    added data-id params

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -34,15 +34,15 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <div class="conferences">
-	{#each conferences as conference}
-		<div class="conference">
+	{#each conferences as conference (conference.id)}
+		<div class="conference" data-id={conference.id}>
 			{conference.name}
 
-			<ul>
-				{#each conference.teams as team}
-					<li class="team">{team.name}</li>
+			<ol>
+				{#each conference.teams as team (team.id)}
+					<li class="team" data-id={team.id}>{team.name}</li>
 				{/each}
-			</ul>
+			</ol>
 		</div>
 	{/each}
 </div>
