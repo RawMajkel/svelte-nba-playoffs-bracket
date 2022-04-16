bb06646866197283f78ece87e4669813d0b3b2d2
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-16 17:33:48 +0200

    changed teamCard into MatchupCard

---
+++ src/components/MatchupCard.svelte
@@ -0,0 +1,22 @@
+<script type="ts">
+	import type { Matchup } from '../common/matchup';
+
+	export let matchup: Matchup;
+</script>
+
+<!-- <div class="team team--{team.abbr.toLowerCase()}" data-id={team.id}>
+	<img class="team__logo" src={team.imgPath} alt={`${team.fullName} logo`} loading="lazy" />
+	<h3 class="team__title">{team.seed}. {team.fullName}</h3>
+</div> -->
+
+<h3 class="team__title">{matchup.higherTeam.seed}. {matchup.higherTeam.fullName}</h3>
+<h3 class="team__title">{matchup.lowerTeam.seed}. {matchup.lowerTeam.fullName}</h3>
+
+<style lang="scss">
+	$team: '.team';
+	#{$team} {
+		&__logo {
+			width: 100px;
+		}
+	}
+</style>
