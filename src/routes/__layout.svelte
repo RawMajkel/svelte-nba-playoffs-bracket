faf730476299d6320be3c7b9974a79a3b0f1eb0c
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-16 15:11:15 +0200

    added roboto font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,5 +3,6 @@
 </div>
 
 <style lang="scss" global>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
 	@import '../../static/globals';
 </style>
