f3e3db4b24d9b55d7883f0da208d9cb38067d528
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-19 17:19:11 +0200

    1.0

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const appConfig = {
 	name: 'SvelteKit NBA Playoffs Bracket',
-	version: '1.0-rc.1',
+	version: '1.0',
 	author: 'Michał Droździk',
 	authorLink: 'https://github.com/RawMajkel',
 	latestEdit: new Date('2022-04-19')
